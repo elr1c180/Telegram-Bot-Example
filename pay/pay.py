0d3fd5e2bec9eac9680276cc44de4488dddaac2d
Anton Elesin <31272958+elr1c180@users.noreply.github.com>
2024-07-06 17:19:40 +0400

    Update pay.py

--- pay/pay.py
+++ pay/pay.py
@@ -3,7 +3,7 @@ from yoomoney import Quickpay
 # Создаем функцию, которая возвращает ссылку на оплату 2р
 def payment_link():
     quickpay = Quickpay(
-                receiver="4100117799284791",
+                receiver="",
                 quickpay_form="shop",
                 targets="Sponsor this project",
                 paymentType="SB",
